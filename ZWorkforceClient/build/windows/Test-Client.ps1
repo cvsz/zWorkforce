b46045c5457da11ef1e46cb01a66f250a460422a
cvsz <seaza@msn.com>
2026-08-09 16:05:23 +0000

    fix: trust MSIX smoke certificate as trusted person

--- ZWorkforceClient/build/windows/Test-Client.ps1
+++ ZWorkforceClient/build/windows/Test-Client.ps1
@@ -90,7 +90,7 @@ if ($LaunchSmoke) {
         if ($null -ne $certificate) {
             Write-Host "Trusting the temporary package certificate for this smoke check."
             $quotedCertificatePath = '"' + $certificate.FullName + '"'
-            Invoke-Certutil @("-user", "-f", "-addstore", "Root", $quotedCertificatePath)
+            Invoke-Certutil @("-user", "-f", "-addstore", "TrustedPeople", $quotedCertificatePath)
             $importedCertificateThumbprint = $certificate.Thumbprint
         }
 
@@ -136,7 +136,7 @@ if ($LaunchSmoke) {
         }
         if ($null -ne $importedCertificateThumbprint) {
             try {
-                Invoke-Certutil @("-user", "-delstore", "Root", $importedCertificateThumbprint)
+                Invoke-Certutil @("-user", "-delstore", "TrustedPeople", $importedCertificateThumbprint)
             } catch {
                 Write-Warning "Could not remove the temporary package certificate: $($_.Exception.Message)"
             }
