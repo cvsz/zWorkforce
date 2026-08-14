8072f5c238e02a56cfac6e785fd6dae685f0e990
cvsz <seaza@msn.com>
2026-08-14 18:30:37 +0700

    remove API version drift from tests

--- tests/test_api_v2.py
+++ tests/test_api_v2.py
@@ -6,6 +6,7 @@ import urllib.error
 from http.server import ThreadingHTTPServer
 
 from common import stack
+from zworkforce import __version__
 from zworkforce.api import App, _sanitize_header_value
 
 
@@ -29,7 +30,7 @@ class ApiV2Tests(unittest.TestCase):
     def test_health_is_public(self):
         with urllib.request.urlopen(self.base+"/health",timeout=5) as r:
             data=json.loads(r.read())
-            self.assertEqual(data["version"],"3.0.2")
+            self.assertEqual(data["version"],__version__)
             self.assertEqual(r.headers["X-Frame-Options"],"DENY")
 
     def test_header_values_strip_response_splitting_bytes(self):
